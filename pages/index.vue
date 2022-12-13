a3870853bfe17ef2ab829bbdcb4733b492085540
Hoang Thi Kieu Phuong <phuong.hoang@dsquare.com.vn>
2022-12-13 15:20:32 +0700

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -75,7 +75,9 @@ const leadingsText = computed(() => [
           </div>
         </div>
         <div class="md:flex md:w-3/8 justify-center items-end relative">
-          <Logo class="absolute -top-64 -right-0 text-white" />
+          <Logo
+            class="absolute top-8 md:-top-64 -right-48 md:-right-0 text-white"
+          />
         </div>
       </PageSection>
     </PageBody>
--- pages/ranking.vue
+++ pages/ranking.vue
@@ -40,25 +40,42 @@ useHead(() => ({
       <PageSection>
         <PageSectionTitle text="Easy" class="capitalize" />
         <div class="">
-          <table width="100%">
-            <thead>
+          <table width="100%" class="border text-center border-green-200">
+            <thead class="border-b border-green-200">
               <tr>
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Score</th>
-                <th>Images</th>
+                <th class="border-r px-6 py-4 bg-green-100 border-green-200">
+                  Rank
+                </th>
+                <th class="border-r px-6 py-4 bg-green-100 border-green-200">
+                  Name
+                </th>
+                <th class="border-r px-6 py-4 bg-green-100 border-green-200">
+                  Score
+                </th>
+                <!-- <th class="border-r px-6 py-4 bg-green-100 border-green-200">
+                  Images
+                </th> -->
               </tr>
             </thead>
             <tbody>
               <tr
                 v-for="(player, index) in playersData.easyPlayersData"
                 :key="index"
+                class="border-b border-green-200"
               >
-                <td>{{ index + 1 }}</td>
-                <td>{{ player.userName }}</td>
-                <td>{{ player.score }}</td>
-                <td>
+                <td class="border-r border-green-200 px-6 py-4">
+                  {{ index + 1 }}
+                </td>
+                <td class="border-r border-green-200 px-6 py-4">
+                  {{ player.userName }}
+                  <a href="#">Images</a>
+                </td>
+                <td class="border-r border-green-200 px-6 py-4">
+                  {{ player.score }}
+                </td>
+                <!-- <td class="border-r border-green-200 px-6 py-4">
                   <div class="flex justify-between py-4">
+
                     <img
                       v-for="(imageData, imageindex) in player.imagesData"
                       :key="imageindex"
@@ -66,7 +83,7 @@ useHead(() => ({
                       class="w-32"
                     />
                   </div>
-                </td>
+                </td> -->
               </tr>
             </tbody>
           </table>
