3f52d552cd01e82eff2ce648a9007866132d8e11
Huynh Thanh Phong <phongjalvn@gmail.com>
2022-12-14 16:00:30 +0700

    change theme default to light

--- utils/theme.ts
+++ utils/theme.ts
@@ -18,7 +18,7 @@ export function ThemeManager() {
 
   // methods
   const getUserSetting = (): IThemeSettingOptions =>
-    themeUserSetting.value || 'system'
+    themeUserSetting.value || 'light'
   const getSystemTheme = (): ITheme => {
     try {
       return window
